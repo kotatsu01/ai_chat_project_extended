ab2ae61e84d5ab4dfb786fc9456d1b69aee0fd30
Sizuku <63285191+kotatsu01@users.noreply.github.com>
2025-07-15 02:14:24 +0900

    Update app.py

--- app.py
+++ app.py
@@ -83,4 +83,6 @@ def feedback():
     return "評価を受け付けました！"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import os
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
